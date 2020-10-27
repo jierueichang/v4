dda088ba736c1aa7b828f0966ff46edf01eb7376
knosmos <jierueic@gmail.com>
2020-10-27 15:45:53 -0400

    Initial Commit

---
+++ assets/nav.js
@@ -0,0 +1,23 @@
+let links = `
+<a href=projects.html>Projects</a>
+<a href=resume.html>Resume</a>
+<a href=journal.html>Journal</a>
+<a href=contact.html>Contact</a>`;
+
+document.getElementById('links').innerHTML = links;
+document.getElementById('mobileNav').innerHTML = links;
+
+let burger = document.getElementById('burger');
+let mobileNav = document.getElementById('mobileNav');
+let closebutton = document.getElementById('closebutton');
+
+burger.onclick = function(){
+    mobileNav.style.top = '0%';
+    closebutton.style.display = 'block';
+    setTimeout(function(){closebutton.style.opacity = '1';},200);
+}
+closebutton.onclick = function(){
+    mobileNav.style.top = '-100%';
+    closebutton.style.opacity = '0';
+    setTimeout(function(){closebutton.style.display = 'none';},200);
+}
